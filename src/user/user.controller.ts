94db9a079f7c1ef6029b85c701276f78fe64fded
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-08-06 06:49:36 +0200

    add update user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,6 +12,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 import { UserService } from './user.service';
 import { CreateUserDTO } from './dtos/create-user';
+import { UpdateUserDTO } from './dtos/update-user';
 
 @ApiTags('User')
 @Controller('users')
@@ -54,6 +55,22 @@ export class UserController {
     @Body() body: CreateUserDTO,
   ) {
     const response = await this.userService.createUser(body);
+    res.status(response.httpStatus).json(response);
+  }
+
+  /**
+   * Updates a  tool
+   * [PUT] /tool
+   */
+  @Post(':id')
+  async updateTool(
+    @Req() _req: Request,
+    @Res() res: Response,
+    @Param('id') id: number,
+    @Body() body: UpdateUserDTO,
+  ) {
+    const data = { user_id: id, ...body };
+    const response = await this.userService.updateTool(data);
     res.status(response.httpStatus).json(response);
   }
 
