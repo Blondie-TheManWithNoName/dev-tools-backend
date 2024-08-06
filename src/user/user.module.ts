2c27e7a2e78095db0e8e0fe6a78182823cdf6e20
Noah <noah.barbera@waapiti.eu>
2024-08-06 16:28:40 +0200

    add favorites endpoints

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,11 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user';
+import { Tool } from 'src/entities/tool';
+import { Favorite } from 'src/entities/favorites';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Tool, Favorite])],
   controllers: [UserController],
   providers: [UserService],
 })
