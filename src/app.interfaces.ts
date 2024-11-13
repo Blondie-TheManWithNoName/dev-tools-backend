881e35c50520b7c479bf807013c73431c40b1333
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-11-13 17:18:55 +0100

    add query filters interface

--- src/app.interfaces.ts
+++ src/app.interfaces.ts
@@ -5,3 +5,14 @@ export interface AuthRequest extends Request {
   /** User of the request */
   readonly user: User;
 }
+
+export interface QueryFilters {
+  /** Number of page */
+  page: number;
+  /** Number of elements per page */
+  take: number;
+  /** Search */
+  search: string;
+  /** Order Direction */
+  orderDir: boolean;
+}
